300fe2685b1a4a0ae912eef1b10fcca536ec3175
Arain <liu2596615@163.com>
2014-12-10 23:40:50 +0800

    Initial Commit

---
+++ gradpro/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  gradpro
+//
+//  Created by Arain on 14/12/10.
+//  Copyright (c) 2014年 Arain. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
